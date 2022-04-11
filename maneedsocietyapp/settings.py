55e3971592bd7e7f5176d18c852fdaec56783f6c
Ben Joseph <benjosephcr7@gmail.com>
2022-04-11 23:28:07 +0530

    chnages

--- maneedsocietyapp/settings.py
+++ maneedsocietyapp/settings.py
@@ -122,10 +122,10 @@ USE_TZ = True
 
 # STATIC_URL = '/static/'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
+STATIC_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT= os.path.join(BASE_DIR,'static')
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
 
