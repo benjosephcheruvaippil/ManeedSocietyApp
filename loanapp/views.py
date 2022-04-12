b15b165a23b24615f8e6584dcafdd8addae47474
Ben Joseph <benjosephcr7@gmail.com>
2022-04-12 16:25:55 +0530

    done the reading

--- loanapp/views.py
+++ loanapp/views.py
@@ -17,21 +17,23 @@ def home_view(request):
         document=FilesUpload.objects.create(file=filename)
         document.save()  #saves the file to media directory
 
-
-        # filename = "home-style.css"
-        # reading from excel
-        # path="..\\maneedsocietyapp\\media\\%s" %(filename)
         path=os.path.join(settings.BASE_DIR,'media/%s' %(filename))
-        if path:
-            print("path",path)
-        else:
-            print("not found")
-        
         
         wb_obj=openpyxl.load_workbook(path)
         sheet_obj = wb_obj.active
+
+        column_nos=sheet_obj.max_column
+        row_nos=sheet_obj.max_row
+        # print("colum numbers",column_nos)
+        # print("row numbers",row_nos)
+        for r in range(row_nos):
+            for c in range(column_nos):
+                if r!=0:
+                    cell_obj = sheet_obj.cell(row = r+1, column = c+1)
+                    print(cell_obj.value)
+
         cell_obj = sheet_obj.cell(row = 2, column = 1)
-        print("Cell value",cell_obj.value)
+        # print("Cell value",cell_obj.value)
         # reading from excel
 
         return HttpResponse("Your file was saved successfully"+"-"+"Cell Value:"+cell_obj.value+".Filename: "+str(filename))
