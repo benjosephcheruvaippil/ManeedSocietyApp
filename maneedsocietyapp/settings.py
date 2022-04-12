7f2a552d734d4ac404f0533cf8c7bbbefbbe38ef
Ben Joseph <benjosephcr7@gmail.com>
2022-04-12 11:29:39 +0530

    again commit for statis files

--- maneedsocietyapp/settings.py
+++ maneedsocietyapp/settings.py
@@ -75,6 +75,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'maneedsocietyapp.wsgi.application'
 
 
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -121,10 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 # STATIC_URL = '/static/'
-STATIC_URL = '/staticfiles/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'staticfiles'),
-# )
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
