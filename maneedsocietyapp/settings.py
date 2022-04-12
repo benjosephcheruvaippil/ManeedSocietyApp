2b212ffdf8ca20c2c98345fb39334cec6b50d9d6
Ben Joseph <benjosephcr7@gmail.com>
2022-04-12 15:02:36 +0530

    sfg

--- maneedsocietyapp/settings.py
+++ maneedsocietyapp/settings.py
@@ -138,8 +138,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
-MEDIA_ROOT=os.path.join(BASE_DIR,'static')
-MEDIA_URL='/static/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'staticfiles')
+MEDIA_URL='/static2/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
