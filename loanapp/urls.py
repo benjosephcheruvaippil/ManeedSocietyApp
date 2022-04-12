b2618a0455305091f9452223f7e14fcd646cec0f
Ben Joseph <benjosephcr7@gmail.com>
2022-04-12 10:36:25 +0530

    staticfiles commit

--- loanapp/urls.py
+++ loanapp/urls.py
@@ -10,6 +10,6 @@ urlpatterns=[
     path('home-view',views.home_view,name='home-view')
 ]
 
-# if settings.DEBUG:
-#     urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-#     urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
