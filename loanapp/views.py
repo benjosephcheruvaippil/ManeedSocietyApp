91cf7ef6b80ce45bd88b34101099f6d5d536cbc1
Ben Joseph <benjosephcr7@gmail.com>
2022-04-12 14:04:24 +0530

    new

--- loanapp/views.py
+++ loanapp/views.py
@@ -9,14 +9,16 @@ import os
 # Create your views here.
 def home_view(request):
     if request.method=="POST":
-        shutil.rmtree("..\\maneedsocietyapp\\media") #removes the directory media and all the files in it.
+        # shutil.rmtree("..\\maneedsocietyapp\\media") #removes the directory media and all the files in it.
         filename=request.FILES["file"]
         print("filename",filename)
         document=FilesUpload.objects.create(file=filename)
         document.save()  #saves the file to media directory
 
         # reading from excel
-        path="..\\maneedsocietyapp\\media\\%s" %(filename)
+        # path="..\\maneedsocietyapp\\media\\%s" %(filename)
+        path="..\\maneedsocietyapp\\staticfiles\\%s" %(filename)
+        
         wb_obj=openpyxl.load_workbook(path)
         sheet_obj = wb_obj.active
         cell_obj = sheet_obj.cell(row = 2, column = 1)
