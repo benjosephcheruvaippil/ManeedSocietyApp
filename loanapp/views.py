759ac2b8222d3e8a4bc35af07b5fe83dee4d2fbd
Ben Joseph <benjosephcr7@gmail.com>
2022-05-13 09:50:28 +0530

    member data added

--- loanapp/views.py
+++ loanapp/views.py
@@ -18,7 +18,6 @@ from loanapp.decorators import check_session
 # @check_session
 def home_view(request):
     if request.method=="POST":
-        # shutil.rmtree("..\\maneedsocietyapp\\media") #removes the directory media and all the files in it.
         shutil.rmtree(os.path.join(settings.BASE_DIR,'media')) #removes the directory media and all the files in it.
         filename=request.FILES["file"]
         print("filename",filename)
@@ -27,18 +26,27 @@ def home_view(request):
 
         path=os.path.join(settings.BASE_DIR,'media/%s' %(filename))
         
-        wb_obj=openpyxl.load_workbook(path)
+        wb_obj=openpyxl.load_workbook(path,data_only=True)
         sheet_obj = wb_obj.active
+
+        # Delete all data from MemberDetails table
+        MemberDetails.objects.all().delete()
+        # Delete all data from MemberDetails table
 
         column_nos=sheet_obj.max_column
         row_nos=sheet_obj.max_row
-        # dict_car={ }
+        insertFlag=1
+
         for r in range(row_nos):
             dict_car={ }
+            insertFlag=1
             for c in range(column_nos):
                 if r!=0:
                     cell_obj = sheet_obj.cell(row = r+1, column = c+1)
                     if c+1==2:
+                        if not cell_obj.value:
+                            insertFlag=0
+                            break
                         dict_car['member_number']=cell_obj.value
                     if c+1==3:
                         dict_car['member_type']=cell_obj.value
@@ -46,9 +54,39 @@ def home_view(request):
                         dict_car['name']=cell_obj.value
                     if c+1==5:
                         dict_car['sex']=cell_obj.value
-            if r!=0:
+                    if c+1==6:
+                        dict_car['father_husband_name']=cell_obj.value
+                    if c+1==7:
+                        dict_car['house_name']=cell_obj.value
+                    if c+1==8:
+                        dict_car['kara']=cell_obj.value
+                    if c+1==9:
+                        dict_car['post']=cell_obj.value
+                    if c+1==10:
+                        dict_car['pin']=cell_obj.value
+                    if c+1==11:
+                        dict_car['village']=cell_obj.value
+                    if c+1==12:
+                        dict_car['taluk']=cell_obj.value
+                    if c+1==13:
+                        dict_car['panchayath']=cell_obj.value
+                    if c+1==14:
+                        dict_car['ward']=cell_obj.value
+                    if c+1==15:
+                        dict_car['mobile_number']=cell_obj.value
+                    if c+1==16:
+                        dict_car['dob']=cell_obj.value
+                    if c+1==17:
+                        dict_car['age']=cell_obj.value
+                    if c+1==18:
+                        dict_car['aadhar']=cell_obj.value
+                    
+            if r!=0 and insertFlag!=0:
                 member_data=MemberDetails.objects.create(
-                    member_number=dict_car['member_number'],member_type=dict_car['member_type'],name=dict_car['name'],sex=dict_car['sex']
+                    member_number=dict_car['member_number'],member_type=dict_car['member_type'],name=dict_car['name'],sex=dict_car['sex'],
+                    father_husband_name=dict_car['father_husband_name'],house_name=dict_car['house_name'],kara=dict_car['kara'],post=dict_car['post'],pin=dict_car['pin'],
+                    village=dict_car['village'],taluk=dict_car['taluk'],panchayath=dict_car['panchayath'],ward=dict_car['ward'],
+                    mobile_number=dict_car['mobile_number'],dob=dict_car['dob'],age=dict_car['age'],aadhar=dict_car['aadhar'],
                 )   
                 print("Member Data",member_data)    
 
@@ -61,30 +99,6 @@ def home_view(request):
     # else:
     #     raise PermissionDenied
 
-
-# class ViewPDF(View):
-#     def get(self,request,*args,**kwargs):
-#         pdf=render_to_pdf("loan-agreement.html",loan_data)
-#         return HttpResponse(pdf,content_type='application/pdf')
-#         # return HttpResponse({"done"})
-
-# def render_to_pdf(template_src,context_dict={}):
-#     template=get_template(template_src)
-#     html=template.render(context_dict)
-#     result=BytesIO()
-#     pdf=pisa.pisaDocument(BytesIO(html.encode("UTF-8")),result)
-#     # pdf=pisa.CreatePDF(html,result,link_callback="",encoding='UTF-8')
-#     if not pdf.err:
-#         return HttpResponse(result.getvalue(),content_type='application/pdf')
-#     return None
-
-# def DownloadHTMLToPDF(request):
-#     pdf=render_to_pdf("loan-agreement.html",loan_data)
-#     response=HttpResponse(pdf,content_type='application/pdf')
-#     filename="LoanAgreement_%s.pdf" %("00001")
-#     content="attachment; filename=%s" %(filename)
-#     response['Content-Disposition']=content
-#     return response
 
 def RenderHTML(request):
     print(request.POST.get('txtVaypa'))
